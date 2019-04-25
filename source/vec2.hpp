ec08acc66ae2d7a64fba10bdfc873e5063ed2408
Simon M <Simon M>
2019-04-25 22:47:45 +0200

    vec2.hpp add test

---
+++ source/vec2.hpp
@@ -0,0 +1,13 @@
+#ifndef VEC2_HPP
+#define VEC2_HPP
+
+// Vec2 data type definition
+struct Vec2
+{
+    float x;
+    float y;
+/* TODO add member variables with
+default member initialisation */
+};
+# endif // VEC2_HPP
+
