e06d6b1c87aea7c066acec7ad575b6b4259e2dab
Simon M <Simon M>
2019-05-03 22:09:44 +0200

    better * and *= mit hilfsvariablen

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -2,10 +2,16 @@
 // TODO (in mat2 .cpp ) Definition v. operator *=
 
 Mat2& Mat2::operator *=( Mat2 const& m){   // Memberfunktion (in .cpp mit Konstr)!!
-    e_00 = e_00*m.e_00 + e_01*m.e_10;       // Frage -warum hier vorm Operator nochmal Mat2:: und in hpp nicht ??
-    e_01 = e_00*m.e_01 + e_01*m.e_11;       // Weil Objekt sich so selberaufruft für die Operation ???
-    e_10 = e_10*m.e_00 + e_11*m.e_10;
-    e_11 = e_10*m.e_01 + e_11*m.e_11; 
+
+    float lo = e_00;               // Hilfsvariablen für Zwischenrechnung
+    float ro = e_01;
+    float lu = e_10;
+    float ru = e_11;
+
+    e_00 = lo*m.e_00 + ro*m.e_10;       // Frage -warum hier vorm Operator nochmal Mat2:: und in hpp nicht ??
+    e_01 = lo*m.e_01 + ro*m.e_11;       // Weil Objekt sich so selberaufruft für die Operation ???
+    e_10 = lu*m.e_00 + ru*m.e_10;
+    e_11 = lu*m.e_01 + ru*m.e_11; 
     return *this;  
 };
 
